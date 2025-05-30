f37297b34ffb8778339d4b8d808e9b13c403b4d7
Brittany-Potato <Brittanycc@missionreadyhq.com>
2025-05-30 20:37:13 +1200

    Deleted back container <3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import styles from './App.module.css'
 import Aicontainer from "./components/ai-container/ai-container";
 import Navbar from "./components/Navbar/navbar";
+import backContainer from './components/back-container/back-container'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className={styles.backContainer}>
       <Navbar />
       <Aicontainer />
-    </>
+      <backContainer/>
+    </div>
   );
 }
 
