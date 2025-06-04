41e621e6624bfc14eb77fc1595e7274b8ceb4559
Cess-stack <celesset@missionreadyhq.com>
2025-06-05 09:31:26 +1200

    -Had to update App.css to make sure layout supports vertical stacking and height management. Lack of flexbox use means no strcture layouts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import styles from './App.module.css'
 import Aicontainer from "./components/ai-container/ai-container";
 import Navbar from "./components/Navbar/navbar";
